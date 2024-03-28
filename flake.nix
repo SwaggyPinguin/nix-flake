1ffc933dd45797e3b31efd768ab94f0d9b57cdd1
noah <noahdahms@gmail.com>
2024-03-28 20:33:49 +0100

    removed stable packages and added modules folder

--- flake.nix
+++ flake.nix
@@ -3,56 +3,46 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-		nixpkgs-stable.url = "nixpkgs/nixos-23.11";
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }: 
-	let
-		systemSettings = {
-			system = "x86_64-linux";
-			hostname = "herbstnix";
-			stateVersion = "23.11";
-		};	
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      systemSettings = {
+        system = "x86_64-linux";
+        hostname = "herbstnix";
+        stateVersion = "23.11";
+      };
 
-		userSettings = {
-			username = "noah";
-			name = "noah";
-			email = "noahdahms@gmail.com";
-		};
+      userSettings = {
+        username = "noah";
+        name = "noah";
+        email = "noahdahms@gmail.com";
+      };
 
-		pkgs = import nixpkgs {
-			system = systemSettings.system;
-			config = {
-				allowUnfree = true;
-				allowUnfreePredicate = (_: true);
-			};
-		};
+      pkgs = import nixpkgs {
+        system = systemSettings.system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
+      };
+    in {
+      formatter.${systemSettings.system} = pkgs.nixfmt;
 
-		pkgs-stable = import nixpkgs-stable {
-			system = systemSettings.system;
-			config = {
-				allowUnfree = true;
-				allowUnfreePredicate = (_: true);
-			};
-		};
-	in
-	{
-		settings.experimental-features = [ "nix-command" "flakes" ];
+      settings.experimental-features = [ "nix-command" "flakes" ];
 
-		homeConfigurations = {
-			user = home-manager.lib.homeManagerConfiguration {
-				inherit pkgs;
-				modules = [ ./home.nix ];
-				extraSpecialArgs = {
-					inherit pkgs-stable;
-					inherit systemSettings;
-					inherit userSettings;
-				};
-			};
-		};
-  };
+      homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ ./home.nix ];
+        extraSpecialArgs = {
+          inherit systemSettings;
+          inherit userSettings;
+        };
+      };
+    };
 }
--- home.nix
+++ home.nix
@@ -1,44 +1,22 @@
-{ pkgs, userSettings, systemSettings, ... }:
-{
-	home = {
-		username = userSettings.username;
-		homeDirectory = "/home/" + userSettings.username;
-		stateVersion = systemSettings.stateVersion;
-		enableNixpkgsReleaseCheck = false;
-	};
+{ pkgs, userSettings, systemSettings, ... }: {
+  home = {
+    username = userSettings.username;
+    homeDirectory = "/home/" + userSettings.username;
+    stateVersion = systemSettings.stateVersion;
+    enableNixpkgsReleaseCheck = false;
+  };
 
-	home.packages = with pkgs; [
-		vim
-		neovim
-		curl
-		wget
-		libgcc
-		zig
-	];
+  imports = [ ./modules ./modules/git.nix ];
 
-	# TODO: move to extra file
-	programs.git = {
-		enable = true;
-		userName = userSettings.name;
-		userEmail = userSettings.email;
-		extraConfig = {
-			core = {
-				editor = "nvim";
-				fileMode = true;
-			};
-			credential.helper = "cache --timeout=86400";
-		};
-	};
+  modules = { herbstluftwm.enable = true; };
 
-	programs.lazygit.enable = true;
+  home.packages = with pkgs; [ vim neovim curl wget libgcc zig ];
 
-	home.sessionVariables = {
-		EDITOR = "nvim";
-	};
+  home.sessionVariables = { EDITOR = "nvim"; };
 
-	xdg.enable = true;
-	xdg.mime.enable = true;
-	xdg.mimeApps.enable = true;
+  xdg.enable = true;
+  xdg.mime.enable = true;
+  xdg.mimeApps.enable = true;
 
-	programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
