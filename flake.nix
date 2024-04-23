710fce7f9e23f8a06c163c535804775979c7fff6
Noah Dahms <noahdahms@gmail.com>
2024-04-23 21:09:22 +0200

    added doom-emacs (not working yet)

--- flake.nix
+++ flake.nix
@@ -8,6 +8,15 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Doom Emacs
+    emacs-pin-nixpkgs.url = "nixpkgs/f72123158996b8d4449de481897d855bc47c7bf6";
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    nix-doom-emacs.inputs.nixpkgs.follows = "emacs-pin-nixpkgs";
+
+    nix-straight.url = "github:librephoenix/nix-straight.el/pgtk-patch";
+    nix-straight.flake = false;
+    nix-doom-emacs.inputs.nix-straight.follows = "nix-straight";
   };
 
   outputs = inputs @ {
@@ -38,12 +47,23 @@
       browser = "google-chrome";
     };
 
-    pkgs = import nixpkgs {
+    # create patched nixpkgs
+    nixpkgs-patched = (import nixpkgs {system = systemSettings.system;}).applyPatches {
+      name = "nixpkgs-patched";
+      src = nixpkgs;
+      patches = [./modules/patches/emacs-no-version-check.patch];
+    };
+
+    pkgs = import nixpkgs-patched {
       system = systemSettings.system;
       config = {
         allowUnfree = true;
         allowUnfreePredicate = _: true;
       };
+    };
+
+    pkgs-emacs = import inputs.emacs-pin-nixpkgs {
+      system = systemSettings.system;
     };
   in {
     formatter.${systemSettings.system} = pkgs.alejandra;
@@ -58,6 +78,7 @@
           inherit inputs;
           inherit systemSettings;
           inherit userSettings;
+          inherit pkgs-emacs;
         };
       };
     };
--- modules/home/opt/emacs/default.nix
+++ modules/home/opt/emacs/default.nix
@@ -2,13 +2,48 @@
   config,
   lib,
   pkgs,
+  inputs,
+  pkgs-emacs,
   ...
 }: {
+  imports = [
+    inputs.nix-doom-emacs.hmModule
+  ];
+
   config = lib.mkIf config.modules.emacs.enable {
-    programs.emacs = {
+    programs.doom-emacs = {
       enable = true;
-      package = pkgs.emacs;
-      # extraConfig = '''';
+      emacsPackage = pkgs-emacs.emacs29-pgtk;
+      doomPrivateDir = ./doom.d;
     };
+
+    # programs.emacs = {
+    #   enable = true;
+    #   package = pkgs.emacs;
+    #   # extraConfig = '''';
+    # };
+
+    home.packages = with pkgs-emacs; [
+    ];
+
+    # home.file.".emacs.d/config.el" = {
+    #   source = "~/.config/doom/config.el";
+    #   recursive = true;
+    # };
+
+    # home.file.".emacs.d/init.el" = {
+    #   source = "~/.config/doom/init.el";
+    #   recursive = true;
+    # };
+
+    # home.file.".emacs.d/packages.el" = {
+    #   source = "~/.config/doom/packages.el";
+    #   recursive = true;
+    # };
+
+    # home.file.".emacs.d/custom.el" = {
+    #   source = "~/.config/doom/custom.el";
+    #   recursive = true;
+    # };
   };
 }
